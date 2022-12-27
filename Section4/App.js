2aadba92236e9a1afb2487699e4d635fc50bc117
Inhoob <inhoob@gmail.com>
2022-12-27 17:59:47 +0900

    배경화면에 그라데이션 설정

--- Section4/App.js
+++ Section4/App.js
@@ -1,17 +1,19 @@
-import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet, Text, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient
+      colors={["#ddb52f", "#4e0429", "#20D173"]}
+      style={styles.rootScreen}
+    >
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
   },
 });
