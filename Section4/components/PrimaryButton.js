44353a62d935e530b492c4ffdeeb8a1a263df58d
Inhoob <inhoob@gmail.com>
2022-12-27 17:45:06 +0900

    button 생성 및 레이아웃

---
+++ Section4/components/PrimaryButton.js
@@ -0,0 +1,43 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("pressed!");
+  }
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.buttonInnercontainer]
+            : styles.buttonInnercontainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#BD4D85" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnercontainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
