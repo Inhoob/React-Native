55c91b9f640a850d4c53f6a1bfca455ea406a788
Inhoob <inhoob@gmail.com>
2022-12-27 18:09:33 +0900

    background 이미지 크기, 스타일, 투명도 조정

--- Section4/App.js
+++ Section4/App.js
@@ -1,13 +1,21 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+
 export default function App() {
   return (
     <LinearGradient
       colors={["#ddb52f", "#4e0429", "#20D173"]}
       style={styles.rootScreen}
     >
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/dices.jpeg")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,5 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
