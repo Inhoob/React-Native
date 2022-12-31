e20d0b906794fb7d3f966c5ec4eb10ef3e3ce0d0
Inhoob <inhoob@gmail.com>
2022-12-31 14:56:08 +0900

    중첩 텍스트 사용

--- Section4/App.js
+++ Section4/App.js
@@ -12,8 +12,8 @@ export default function App() {
   const [gameIsOver, setGameIsOver] = useState(true);
 
   const [fontsLoaded] = useFonts({
-    "open-sans": require("./assets/fonts/OpenSans-Bold.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
 
   if (!fontsLoaded) {
