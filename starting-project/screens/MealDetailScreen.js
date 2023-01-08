83974d002662528fe1da50f662510c52f4485122
Inhoob <inhoob@gmail.com>
2023-01-09 01:41:53 +0900

    음식 상세 화면에 콘텐츠 추가

--- starting-project/screens/MealDetailScreen.js
+++ starting-project/screens/MealDetailScreen.js
@@ -1,6 +1,25 @@
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
+import MealDetails from "../components/MealDetails";
+import { MEALS } from "../data/dummy-data";
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
-  return <Text>This is detail{mealId}</Text>;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>ingredients</Text>
+      {selectedMeal.ingredients.map((step) => {
+        return <Text key={step}>{step}</Text>;
+      })}
+      <Text>Steps</Text>
+    </View>
+  );
 }
 export default MealDetailScreen;
