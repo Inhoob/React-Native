1cc041171ef2aa5af54387aee05bb177e92c181b
Inhoob <inhoob@gmail.com>
2023-02-01 01:37:52 +0900

    인증 토큰 추출

--- Authentication/util/Auth.js
+++ Authentication/util/Auth.js
@@ -13,12 +13,17 @@ export async function authenticate(mode, email, password) {
 
   // console.log(response.data);
   // 이렇게 하면 로그인 성공 시 displayName,email,expiresIn,idToken 등 id 객체가 뜬다
+
+  const token = response.data.idToken;
+  return token;
 }
 
 export async function createUser(email, password) {
-  await authenticate("signUp", email, password);
+  const token = await authenticate("signUp", email, password);
+  return token;
 }
 
 export async function login(email, password) {
-  await authenticate("signInWithPassword", email, password);
+  const token = await authenticate("signInWithPassword", email, password);
+  return token;
 }
